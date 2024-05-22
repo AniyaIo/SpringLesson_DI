0f8725c0b47f431241989f0b1b5510db1ed249a3
aniya <24aniya_io@axiz.co.jp>
2024-05-22 15:02:19 +0900

    SpringのtomcatによるAPサーバ構築

--- src/main/java/com/example/lesson/LessonApplication.java
+++ src/main/java/com/example/lesson/LessonApplication.java
@@ -11,29 +11,7 @@ public class LessonApplication {
 		var context = SpringApplication.run(LessonApplication.class, args);
 		var userService = context.getBean(IProductService.class);
 
-		//findAll
-		System.out.println("findAll");
-		var list = userService.findAll();
-		list.stream().forEach(System.out::println);
 
-		//findById
-		System.out.println("findById");
-		System.out.println(userService.findById(2));
-
-
-		//insert
-		System.out.println("insert");
-		System.out.println("実行件数:"+userService.insert(new ProductRecord(1,"蛍光ペン(赤)",80)));
-
-		//update
-		System.out.println("update");
-		System.out.println("ID有りの場合:"+userService.update(new ProductRecord(2,"蛍光ペン(緑)",80)));
-		System.out.println("ID無しの場合:"+userService.update(new ProductRecord(5555,"蛍光ペン(緑)",80)));
-
-		//delete
-		System.out.println("delete");
-		System.out.println("ID有りの場合:"+userService.delete(4));
-		System.out.println("ID無しの場合:"+userService.delete(5555));
 	}
 
 }
---
+++ src/main/java/com/example/lesson/ProductController.java
@@ -0,0 +1,26 @@
+package com.example.lesson;
+
+import com.example.lesson.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+    @GetMapping("product-list")
+    public String productList(Model model) {
+        model.addAttribute("productList",productService.findAll());
+        return "product-list";
+    }
+    @GetMapping("product/{id}")
+    public String productById(@PathVariable("id") int id,Model model) {
+        model.addAttribute("product",productService.findById(id));
+        return "product";
+    }
+}
