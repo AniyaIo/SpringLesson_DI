98eccb6fc830044a1e3e83f3321849730daa9805
aniya <24aniya_io@axiz.co.jp>
2024-05-24 19:33:27 +0900

    商品情報の更新と削除機能のアップデート

--- src/main/java/com/example/lesson/ProductController.java
+++ src/main/java/com/example/lesson/ProductController.java
@@ -1,12 +1,13 @@
 package com.example.lesson;
 
+import com.example.lesson.form.AddForm;
 import com.example.lesson.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -24,9 +25,58 @@ public class ProductController {
         return "product";
     }
 
-    @GetMapping("product-add")
-    public String productAdd(Model model) {
-        model.addAttribute("productList",productService.findAll());
-        return "product-list";
+    @GetMapping("/product-add") //product-add.htmlへ飛ぶ
+    public String indexProductAdd(@ModelAttribute("addForm") AddForm addForm) {
+        return "product-add";
+    }
+    @PostMapping("/product-add")
+    public String productAdd(@Validated @ModelAttribute("addForm") AddForm addForm,
+                             BindingResult errorResult) {
+        if(errorResult.hasErrors()) {
+            return "product-add";
+        }
+        productService.insert(
+                new ProductRecord(
+                        1,  //idの値は連番なので無視される
+                        addForm.getName(),
+                        Integer.parseInt(addForm.getPrice())));
+        return "redirect:product-list";
+    }
+
+    @GetMapping("product/update/{id}")
+    public String indexProductUpdate(@PathVariable("id") int id,
+                                     Model model,
+                                     @ModelAttribute("updateForm") AddForm updateForm) {
+        model.addAttribute("product",productService.findById(id));
+        return "update";
+    }
+
+    @PostMapping("product/update/{id}")
+    public String productUpdate(@PathVariable("id") int id,
+                                Model model,
+                                @Validated @ModelAttribute("updateForm") AddForm updateForm,
+                                BindingResult errorResult) {
+        if(errorResult.hasErrors()) {
+            model.addAttribute("product",productService.findById(id));
+            return "update";
+        }
+        productService.update(
+                new ProductRecord(
+                        id,
+                        updateForm.getName(),
+                        Integer.parseInt(updateForm.getPrice())));
+        return "redirect:/product-list";
+    }
+
+    @RequestMapping(value = "edit" , params = "update", method = RequestMethod.POST)
+    public String productUpdate(@ModelAttribute("editForm") int id) {
+        //productService.update(id);
+        return "redirect:/product/update/"+id;
+    }
+
+    @RequestMapping(value = "edit" , params = "delete", method = RequestMethod.POST)
+    public String productDelete(@ModelAttribute("editForm") int id) {
+        productService.delete(id);
+        return "redirect:/product-list";
     }
 }
--- src/main/java/com/example/lesson/dao/ProductDao.java
+++ src/main/java/com/example/lesson/dao/ProductDao.java
@@ -1,16 +1,12 @@
 package com.example.lesson.dao;
 
 import com.example.lesson.ProductRecord;
-import com.example.lesson.dao.IProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.jdbc.core.DataClassRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
